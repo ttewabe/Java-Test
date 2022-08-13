89ccb894988a663be7eb67f163a8d475d5d2b51c
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-08-13 10:57:56 +0000

    \n for new line

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    String message = "C.\\Website Developer\\...";
+    String message = "C.\nWebsite Developer\\...";
        System.out.println(message);
   }
 }

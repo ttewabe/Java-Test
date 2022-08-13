7a8a83002c78f69920641b3ffe3d1509cd4f7e77
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-08-13 10:13:18 +0000

    String

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String message = "I am An Engineer and Website Developer"  + "!!";
+       System.out.println(message);
   }
 }

9971a7eec8dfee4d06dcb0066fe402c5bc6f6717
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-08-13 10:49:58 +0000

    Trim

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    String message = "I am An Engineer and Website Developer"  + "!!";
-       System.out.println(message.toLowerCase());
+    String message = "   I am An Engineer and Website Developer"  + "!!    ";
+       System.out.println(message.trim());
     System.out.println(message);
   }
 }

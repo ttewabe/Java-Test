95111234bd746eb7b41f30e24ae07c1222c38d87
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-08-13 10:48:16 +0000

    toLowerCase

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args) {
     String message = "I am An Engineer and Website Developer"  + "!!";
        System.out.println(message.toLowerCase());
+    System.out.println(message);
   }
 }

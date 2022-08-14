eb586fc9b0d262be42eb6531c4ca37868fe7c2b6
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-08-14 01:29:40 +0000

    Math.ceil

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    int result= (int)Math.floor(1.1F);
+    int result= (int)Math.ceil(1.1F);
        System.out.println(result);
   }
 }

6c89cb2c874e6c9d9e745e990be6a86ce4835b23
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-08-14 02:20:46 +0000

    int used for remove decimals

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    double result= Math.round(Math.random()*100);
+    int result= (int)Math.round(Math.random()*100);
        System.out.println(result);
   }
 }

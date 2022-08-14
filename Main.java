a70a4a253e77016d7b14f5f7af4c2ad7f03cdde5
Tewabe T <32206949+ttewabe@users.noreply.github.com>
2022-08-14 00:46:01 +0000

    Arthimetics operation

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
-    String message = "C.\nWebsite Developer\\...";
-       System.out.println(message);
+    double numbers = (double)10/(double)3;
+       System.out.println(numbers);
   }
 }
